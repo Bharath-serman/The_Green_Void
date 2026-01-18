111aa404b7370191c8ef424e04a2673bdccf6220
Bharath serman R <bhar22005.cd@rmkec.ac.in>
2026-01-18 12:29:46 +0530

    Started Intro

---
+++ Assets/Scripts/GhostBlinking.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using JetBrains.Annotations;
+
+public class GhostBlinking : MonoBehaviour
+{
+    //Get the Inputs
+    [Header("Ghost Images")]
+    [SerializeField] private Image[] GhostImages;
+    //Delay rate
+    private float mindelay = 0.03f;
+    private float maxdelay = 0.3f;
+
+    private bool Status = false;
+    private Coroutine BlinkRoutine;
+
+    //Start the Coroutine.
+    public void BeginCoroutine()
+    {
+        if(BlinkRoutine == null)
+        {
+            BlinkRoutine = StartCoroutine(GhostBlink());
+        }
+    }
+
+    //Stop the Coroutine.
+    public void EndCoroutine()
+    {
+        if(BlinkRoutine != null)
+        {
+            StopCoroutine(BlinkRoutine);
+            BlinkRoutine = null;
+        }
+
+
+        //Iterate through the Image array.
+        foreach(Image image in GhostImages)
+        {
+            image.enabled = Status;  //False.
+        }
+    }
+
+    //Coroutine.
+    IEnumerator GhostBlink()
+    {
+        bool truestatus = true;
+        while (truestatus)  //True.
+        {
+            Image image = GhostImages[Random.Range(0, GhostImages.Length)];
+
+            image.enabled = truestatus;  //True.
+            yield return new WaitForSeconds(Random.Range(mindelay, maxdelay));
+
+            image.enabled = false;
+            yield return new WaitForSeconds(Random.Range(mindelay, maxdelay));
+        }
+    }
+}
