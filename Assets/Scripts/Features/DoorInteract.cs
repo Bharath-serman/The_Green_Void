7c104a68ababde8616f7554085c9977a7a5f4d63
Bharath-serman <bharathserman@gmail.com>
2026-02-13 12:34:25 +0530

    Added Occlusion Culling and Added the Document_SO

--- Assets/Scripts/Features/DoorInteract.cs
+++ Assets/Scripts/Features/DoorInteract.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
-
+using System.Collections;
 public enum DoorType
 {
     FrontDoor,
     InteriorDoor
 }
-
 public class DoorInteract : MonoBehaviour
 {
+    #region Inputs
     //Get the Inputs
     public Animator DoorAnimator;
     private bool IsOpened = false;
@@ -21,15 +21,39 @@ public class DoorInteract : MonoBehaviour
     private string TagName = "Player";
     public DoorType type;
 
+    [Header("TutorialUI")]
+    [SerializeField] GameObject TutorialUI;
+    private bool UIStatus = true;
+    //private float duration = 5f;
+    private bool TutorialShown = false;
+    #endregion
+
+    private void Start()
+    {
+        TutorialUI.SetActive(!UIStatus);  //False.
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(TagName))
+            return;
+
+        if (TutorialShown)
+            return;
+
+        TutorialShown = true;
+        StartCoroutine(DoorTutorialUI(4f));
+    }
+
     //Check for the trigger
     private void OnTriggerStay(Collider other)
     {
         //Check for conditions
-        if (!other.CompareTag(TagName))
+        if (!other.CompareTag(TagName))  //If it is not the player.
             return;
         if (IsOpened)
             return;
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)) 
             DoorOpen();
     }
 
@@ -54,5 +78,22 @@ public class DoorInteract : MonoBehaviour
         }
         IsOpened = true;
     }
+    #endregion
+
+    #region DoorControlUI
+
+    IEnumerator DoorTutorialUI(float duration)
+    {
+        //Show the UI.
+        TutorialUI.SetActive(UIStatus);  //True.
+
+        //Wait for X seconds.
+        yield return new WaitForSeconds(duration);
+
+        //Disable the UI.
+        TutorialUI.SetActive(!UIStatus);  //False.
+
+    }
+
     #endregion
 }
